bf5161e0f6045c60d2d37b29e7941ef0ce0d1cb3
Adam Orczyk <adam.orczyk@gmail.com>
2022-04-08 18:09:15 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -42,156 +42,103 @@ input.onButtonPressed(Button.AB, function () {
 class Sensor {
     public key: string;
     public getData: () => number;
-    private interval: number;
+    public interval: number;
     public lastCheck: number;
     public value: number;
     public lastValue: number;
-    private status: boolean;
+    public status: boolean;
     private isRunning: boolean;
-    private delta: number;
+    public delta: number;
     private condition: (value: number, lastValue: number) => boolean;
-    private onChange: (sensor: Sensor) => void;
 
-    constructor(key: string, interval: number, getData: () => number, onChange: (sensor: Sensor) => void, delta: number = null, condition: (value: number, lastValue: number) => boolean = null) {
+    constructor(key: string, interval: number, getData: () => number, delta: number = null, condition: (value: number, lastValue: number) => boolean = null) {
         this.key = key;
         this.interval = interval;
         this.getData = getData;
         this.delta = delta;
         this.condition = condition;
-        this.onChange = onChange;
-    }
-
-    public setInterval(interval: number): void {
-        this.interval = interval;
-        bluetooth.uartWriteString(this.key + '- interval: ' + interval + '\n')
-    }
-
-    public setDelta(delta: number): void {
-        this.delta = delta;
-        bluetooth.uartWriteString(this.key + '- delta: ' + delta + '\n')
-    }
-
-    public stop(): void {
-        this.status = false;
-        bluetooth.uartWriteString(this.key + '- stop\n')
-
-    }
-
-    public start(): void {
-        bluetooth.uartWriteString(this.key + '- start\n')
-
+        this.key = key;
+        this.lastCheck = input.runningTime();
         this.status = true;
-        this.isRunning = true;
-        this.value = null;
-
-        control.runInBackground(() => {
-            while (this.status) {
-                let getDataStart = input.runningTime();
-                let value = this.getData()
-                let getDataStop = input.runningTime();
-
-                this.lastCheck = getDataStart
-                let changed = true;
-
-                if (this.delta != null && this.value != null){
-                    if (value > (this.value + this.delta) || value < (this.value - this.delta)) {
-                        this.value = value;
-                    } else {
-                        changed = false;
-                    }
-                } else {
-                    this.value = value;
-                }
-
-                if (typeof this.condition == 'function' && !this.condition(this.value, this.lastValue)){
-                    changed = false;
-                }
-
-                if (changed && typeof this.onChange == 'function') {
-                    control.runInBackground(() => {
-                        this.onChange(this)
-                    })
-                    this.lastValue = this.value
-                }
-
-                basic.pause(this.interval)
-            }
-
-            this.isRunning = false;
-            bluetooth.uartWriteString(this.key + '- stopped\n')
-        })
     }
-}
 
-// let messageQueue: string[] = [];
-// let messageQueueRunning = false;
+    public check(): boolean {
+        this.lastValue = this.value
+        let value = this.getData()
 
-// function sendMessage(message: string){
-//     messageQueue.push(message);
-    
-//     if (!messageQueueRunning){
-//         messageQueueRunning = true;
-//         control.runInBackground(() => {
-//             while (messageQueue.length) {
-//                 let message = messageQueue.shift()
+        this.lastCheck = input.runningTime();
+        let changed = true;
 
-//                 if (sendBluetooth) {
-//                     bluetooth.uartWriteString(message)
-//                 } else if (sendUSB) {
-//                     // serial.writeNumbers([value, this.lastCheck, getDataStop - getDataStart])
-//                     serial.writeString(message)
-//                 }
+        if (this.delta != null && this.value != null){
+            if (value > (this.value + this.delta) || value < (this.value - this.delta)) {
+                this.value = value;
+            } else {
+                changed = false;
+            }
+        } else {
+            this.value = value;
+        }
 
-//                 basic.pause(10);
-//             }
+        if (typeof this.condition == 'function' && !this.condition(this.value, this.lastValue)){
+            changed = false;
+        }
 
-//             messageQueueRunning = false;
-//         })
-//     }
-// }
-
-function sendData(sensor: Sensor){
-    // let out = [sensor.key, sensor.value, sensor.lastCheck].join(';') + '\n'
-    let out = [sensor.key, sensor.value, sensor.lastCheck].join(';') + '\n'
-    if (sendBluetooth) {
-        bluetooth.uartWriteString(out)
-    } else if (sendUSB) {
-        // serial.writeNumbers([value, this.lastCheck, getDataStop - getDataStart])
-        serial.writeString(out)
+        return changed;
     }
 }
 
 let measurements: Sensor[] = [];
-
-// Temperature
-measurements.push(new Sensor('temp', 2000, () => {
-    return input.temperature()
-}, sendData, 0))
-
-// Light
-measurements.push(new Sensor('light', 1000, () => {
-    return input.lightLevel()
-}, sendData, 10))
-
-// Light
-measurements.push(new Sensor('ax', 1000, () => {
-    return input.acceleration(Dimension.X)
-}, sendData, 10))
-
-// Sonar
-// measurements.push(new Sensor('sonar', 500, () => {
-//     return sonar.ping(DigitalPin.P2, DigitalPin.P1, PingUnit.Centimeters)
-// }))
-
-for (let sensor of measurements){
-    sensor.start()
-}
 
 function getSesorByKey(key: string): Sensor {
     return measurements.filter(s => {
         return s.key == key
     })[0]
 }
+
+basic.forever(() => {
+    let changed = false;
+    let now = input.runningTime();
+    let out: number[] = [now]
+
+    for (let sensor of measurements) {
+        // serial.writeString([now, sensor.status ? 1 : 0, sensor.interval, sensor.lastCheck].join(',') + '\n')
+        if (sensor.status && now >= (sensor.interval + sensor.lastCheck)) {
+            changed = changed || sensor.check()
+        }
+
+        out.push(sensor.value)
+    }
+
+    if (changed) {
+        serial.writeNumbers(out)
+        if (sendBluetooth) {
+            bluetooth.uartWriteString(out.join(',') + '\n')
+        } else if (sendUSB) {
+            serial.writeNumbers(out)
+        }
+    }
+})
+
+// Temperature
+measurements.push(new Sensor('temp', 2000, () => {
+    return input.temperature()
+}, 0))
+
+// Light
+measurements.push(new Sensor('light', 1000, () => {
+    return input.lightLevel()
+}, 10))
+
+// Light
+measurements.push(new Sensor('ax', 1000, () => {
+    return input.acceleration(Dimension.X)
+}, 10))
+
+// Sonar
+// measurements.push(new Sensor('sonar', 500, () => {
+//     return sonar.ping(DigitalPin.P2, DigitalPin.P1, PingUnit.Centimeters)
+// }))
+
 
 function messageHandler(receivedString: String){
     let data = receivedString.split(';')
@@ -203,7 +150,7 @@ function messageHandler(receivedString: String){
     }
 
     if (data[0] == 'WebUSB') {
-        if (data[1] == 'on') {
+        if (+data[1]) {
             sendUSB = true;
         } else {
             sendUSB = false;
@@ -213,26 +160,23 @@ function messageHandler(receivedString: String){
 
     let sensor = getSesorByKey(data[0]);
     if (sensor) {
-        if (data[1] == 'interval'){
-            sensor.setInterval(+data[2])
-        } else if (data[1] == 'delta') {
-            if (data[2] == ''){
-                sensor.setDelta(null)
-            } else {
-                sensor.setDelta(+data[2])
-            }
-        } else if (data[1] == 'status') {
-            let status = data[2]
-            if (status == 'on'){
-                sensor.start()
-            } else {
-                sensor.stop()
-            }
-            
-        } else if (data[1] == 'get') {
-            let out = sensor.getData() + '\n';
-            bluetooth.uartWriteString(out)
+        let status = data[1]
+        if (+status) {
+            sensor.status = true
+        } else {
+            sensor.status = false
         }
+
+        if (data.length > 2){
+            sensor.interval = +data[2];
+
+            if (data[3] == '') {
+                sensor.delta = null
+            } else {
+                sensor.delta = +data[3]
+            }
+        }
+
         return
     }
 
