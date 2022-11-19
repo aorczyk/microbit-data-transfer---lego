d70edd02f25be3c815b43b28bc1f3b0688ce8cfd
Adam Orczyk <adam.orczyk@gmail.com>
2022-11-19 19:49:00 +0100

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -239,37 +239,6 @@ function messageHandler(receivedString: String){
 
         return
     }
-
-    if (data.length == 1) {
-        // music.playTone(Note.C, music.beat())
-        basic.showString(data[0])
-        return
-    }
-
-    if (lastReceivedString != receivedString) {
-        lastReceivedString = receivedString
-        basic.clearScreen()
-
-        if (receivedString == "38;") {
-            led.plot(2, 0)
-        } else if (receivedString == "39;") {
-            led.plot(4, 2)
-        } else if (receivedString == "37;") {
-            led.plot(0, 2)
-        } else if (receivedString == "40;") {
-            led.plot(2, 4)
-        } else if (receivedString == "38;39;") {
-            led.plot(4, 0)
-        } else if (receivedString == "37;38;") {
-            led.plot(0, 0)
-        } else if (receivedString == "37;40;") {
-            led.plot(0, 4)
-        } else if (receivedString == "39;40;") {
-           led.plot(4, 4)
-        } else {
-            led.plot(2, 2)
-        }
-    }
 }
 
 serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
